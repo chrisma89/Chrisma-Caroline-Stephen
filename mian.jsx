48a6576a2530d1b9b2dae3f22a3e9ecbce0faa2f
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-16 19:59:07 +0000

    main.jsx file added and content poplulated

---
+++ mian.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import  ReactDOM  from "ReactDOM/client";
+import App from "App";
+
+
+const root = ReactDOM.createroot(document.getElementById("root"))
+
+root.render(
+  <React.StrictMode>
+  <App />
+  </React.StrictMode>
+)
+
