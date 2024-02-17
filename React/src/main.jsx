54ce91eb0282e206e34bf8e535a8b8959b5910f0
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-17 22:04:28 +0000

    main.jsx syntax errors fixed

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import  ReactDOM  from "ReactDOM/client";
-import App from "App";
+import  ReactDOM  from "react-dom/client";
+import App from "./App.jsx";
 
 
-const root = ReactDOM.createroot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root"))
 
 root.render(
   <React.StrictMode>
