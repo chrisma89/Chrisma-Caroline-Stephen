6213ddb1e98b79738f63ce21043dd8df88d0d63d
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-19 15:54:42 +0000

    google fonts added- not working

--- React/src/Pages/HomeCard/index.jsx
+++ React/src/Pages/HomeCard/index.jsx
@@ -10,11 +10,11 @@ function HomeCard() {
     <div className="container-fluid homecard">
     <div className="justify-content-center">
         <div className="text-center title" style={{color : "white", fontSize : "45px", padding : "20px"}}>CHRISMA CAROLINE STEPHEN </div>
-        <div className="text-center" style={{color : "white", fontSize : "35px", padding : "5px"}}>
+        <div className="text-center" style={{color : "white", fontSize : "35px", padding : "1px"}}>
           PORTFOLIO
         </div>
         <Card.Body style={{ backgroundColor: "black", display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}> 
-          <Card.Img className="rounded-circle mx-auto" style={{ width: '60%', height: '60%' }} variant="top" src="src/images/pic.jpeg" /> 
+          <Card.Img className="rounded-circle mx-auto" style={{ width: '60%', height: '60%', border: "25" }} variant="top" src="src/images/pic.jpeg" /> 
         </Card.Body>
     </div>
     </div>
