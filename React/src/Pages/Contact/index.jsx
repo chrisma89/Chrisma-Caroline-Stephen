9d3d81056d92001abd03777cb1c5c5ee9364101a
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-19 11:48:03 +0000

    contact form input field and header styled

--- React/src/Pages/Contact/index.jsx
+++ React/src/Pages/Contact/index.jsx
@@ -1,16 +1,21 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import "./style.css"
 
 
 const Contact = () => {
   return(
     <>
-    <h3>Contact Me</h3>
+    <h3 style={{fontSize: "25px", padding: "20px", color : "#59B4C3",  padding: "50px"}}>Contact Me</h3>
 
 
     {/* Contact form */}
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form  style={{
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+    }}>
+      <Form.Group className="mb-3 form d-flex justify-content-center" controlId="formBasicEmail">
         <Form.Label>First Name</Form.Label>
         <Form.Control type="firstname" placeholder="Enter First Name" />
         <Form.Label>Last Name</Form.Label>
