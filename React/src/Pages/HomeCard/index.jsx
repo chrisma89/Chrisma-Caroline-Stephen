de3d6c63f7947e4f5dd19ddcfc9cc2bafa45f6cf
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-19 02:00:10 +0000

    navbar text size and padding reduced, card background and text color styled

--- React/src/Pages/HomeCard/index.jsx
+++ React/src/Pages/HomeCard/index.jsx
@@ -6,23 +6,13 @@ import "./style.css";
 
 function HomeCard() {
   return (
-    // <Container className="container homecard">
-    //   <h1 className="text-center">CHRISMA CAROLINE STEPHEN</h1>
-    //   <p className="text-center">PORTFOLIO</p>
-
-    //   <Card > 
-    //     <Card.Img className="mx-auto rounded-circle image"
-    //       variant="top"
-    //       src="src\images\pic.jpeg"
-    //     />
-    //   </Card>
-    // </Container>
+  
     <div className="container-fluid homecard">
     <Card className="mx-auto justify-content-center"style={{ width: '24rem' }}>
       
-      <Card.Body>
-        <Card.Title className="text-center">CHRISMA CAROLINE STEPHEN</Card.Title>
-        <Card.Text className="text-center">
+      <Card.Body style={{backgroundColor : "#59B4C3"}}>
+        <Card.Title className="text-center title" style={{color : "white"}}>CHRISMA CAROLINE STEPHEN</Card.Title>
+        <Card.Text className="text-center" style={{color : "white"}}>
           PORTFOLIO
         </Card.Text>
         <Card.Img className="rounded-circle" variant="top" src="src\images\pic.jpeg" />
