6020bb621e43c669287d5456695a835e17a63933
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-19 00:28:41 +0000

    usestate imported, displayproject function added, click event added to project card- but not working yet

--- React/src/Pages/ProjectGallery/index.jsx
+++ React/src/Pages/ProjectGallery/index.jsx
@@ -1,7 +1,21 @@
+import React, { useState } from 'react';
 import Projects from "../../components/Projects";
-import projectsData from "../../projects.json"; // Adjust the path as necessary
+import projectsData from "../../projects.json"; 
 
-const ProjectsGallery = (props) => {
+const ProjectsGallery = () => {
+
+ 
+    // Setting initial state to the projectsdata json array
+    const [projects, setProjects] = useState(projectsData);
+  
+    const displayProject = (id) => {
+      // Display only the project that has been clicked
+      const newProject = projects.filter((project) => project.id === id);
+      // Set friends equal to the new friends array
+      setProjects(newProject);
+    };
+
+
   return(
     <>
     <p>Project Gallery</p>
@@ -14,7 +28,8 @@ const ProjectsGallery = (props) => {
         image ={project.image}
         name = {project.name}
         github={project.github}
-        deployed = {project.deployed}>
+        deployed = {project.deployed}
+        displayProject = {displayProject}>
 
         </Projects>
       ))
--- React/src/components/Projects/index.jsx
+++ React/src/components/Projects/index.jsx
@@ -1,12 +1,8 @@
-// add all 6 projects
-// import projects
-// import 
-// import project from './projects.json';
-// import ProjectGallery from '../../Pages/ProjectGallery';
+
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
-function Projects ({id,image,name, github,deployed}) {
+function Projects ({id,image,name, github,deployed, displayProject}) {
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={image} />
@@ -16,8 +12,11 @@ function Projects ({id,image,name, github,deployed}) {
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <a href={github}><Button variant="primary">GITHUB</Button></a>
-        <a href={deployed}><Button variant="primary">APPLICATION</Button></a>
+        <a href={github} target="_blank"><Button variant="primary">GITHUB</Button></a>
+        <a href={deployed} target="_blank"><Button variant="primary">APPLICATION</Button></a>
+        <Button id={id} onClick={() => displayProject(id)} className="btn-primary">
+        View Project
+      </Button>
       </Card.Body>
     </Card>
   );
