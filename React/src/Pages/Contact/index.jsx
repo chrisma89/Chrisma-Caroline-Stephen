e852947478389a5f3a9c717b01ed1ad87b378795
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-02-19 15:32:23 +0000

    conatct form input field, submit handler and alert added

--- React/src/Pages/Contact/index.jsx
+++ React/src/Pages/Contact/index.jsx
@@ -1,16 +1,52 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import "./style.css"
+import React, { useState } from 'react';
 
 
 const Contact = () => {
+
+  // Setting the component's initial state
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target; 
+  setFormData({
+    ...formData, 
+    [name]: value, 
+  });
+  }
+
+  const handleFormSubmit = (event) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!formData.firstName || !formData.lastName || !formData.email) {
+      alert('Fill out your first and last name please!');
+    } 
+     else {
+      alert(`Hello ${formData.firstName} ${formData.lastName}, Thank you!`);
+    }
+
+    setFormData({
+      firstName: '',
+      lastName: '',
+      email: '',
+    })
+  }
+
+
+
   return(
     <div style={{background : 'black', paddingBottom : "100%"}}>
     <h3 style={{fontSize: "25px", padding: "20px", color : "#59B4C3",  padding: "50px", background : 'black'}}>Contact Me</h3>
 
 
     {/* Contact form */}
-    <Form  style={{
+    <Form onSubmit={handleFormSubmit} style={{
       display: "flex", 
       justifyContent: "center", 
       flexDirection : "column",
@@ -20,13 +56,24 @@ const Contact = () => {
     }}>
       <Form.Group className="mb-3 form d-flex justify-content-center" controlId="formBasicEmail">
         <Form.Label>First Name</Form.Label>
-        <Form.Control type="firstname" placeholder="Enter First Name" />
+        <Form.Control  value={formData.firstName}
+          name="firstName"
+          onChange={handleInputChange}
+          type="text" 
+          placeholder="Enter First Name" />
         <Form.Label>Last Name</Form.Label>
-        <Form.Control type="lastname" placeholder="Enter Last Name" />
+        <Form.Control  value={formData.lastName}
+          name="lastName"
+          onChange={handleInputChange}
+          type="text" placeholder="Enter Last Name" />
         <Form.Label>Email Address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control  value={formData.email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Enter email" />
       </Form.Group>
-      <Button  style ={{backgroundColor : "#59B4C3", margin: "35px"}} type="submit" onSubmit="">
+      <Button type ="submit" style ={{backgroundColor : "#59B4C3", margin: "35px"}} >
         Submit
       </Button>
     </Form>
