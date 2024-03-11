2bde160f602372afc17cd7df65be03c22c93bf08
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-11 23:12:40 +0000

    cv path adjusted

--- src/components/LInks/index.jsx
+++ src/components/LInks/index.jsx
@@ -12,7 +12,7 @@ const Links = () => {
     <a href="https://www.linkedin.com/in/chrisma-caroline-stephen/" target="_blank" className='icon'><FontAwesomeIcon icon={faLinkedin} /></a>
     <a href="https://github.com/chrisma89"  target="_blank" className='icon'><FontAwesomeIcon icon={faGithub} /></a>
     <a href="mailto:chrismacaroline@gmail.com"  target="_blank" className='icon'><FontAwesomeIcon icon={faEnvelope} /></a>
-    <a href="/public/images/CV.pdf"  target="_blank" className='icon'><FontAwesomeIcon icon={faFile} /></a>
+    <a href="/images/CV.pdf"  target="_blank" className='icon'><FontAwesomeIcon icon={faFile} /></a>
 
    </div>
   )
