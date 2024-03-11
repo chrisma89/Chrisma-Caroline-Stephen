567664b6ef87f0f26d00fc018d7974e716684785
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-11 22:59:27 +0000

    toogle button color changed

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,15 +3,21 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import './style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function NavBar() {
   return (
     <Navbar expand="lg" className="container-fluid navbarmain">
       <Container className='justify-content-end'>
       <Navbar.Brand href="#home"></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler">
+        <FontAwesomeIcon icon={faBars} style={{ color: 'white' }} />
+</Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav" className="custom-toggler">
+          <Nav>
             {/* <Link><img  style={{height :"100%", width : "40%"}}src="/images/logo.png"/></Link> */}
           <Link style={{color:"white", paddingRight :"30px", fontFamily : "roboto-black-italic",textDecoration: "none"}} to="/">HOME</Link>
             <Link style={{color:"white", paddingRight :"30px",fontFamily : "roboto-black-italic",textDecoration: "none"}} to="/projects">PROJECTS</Link>
