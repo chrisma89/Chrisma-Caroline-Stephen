c96091d4ac7f5fb7ddd637e5d3fd4a150dde30c4
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-11 23:07:46 +0000

    links page- cv file path changed

--- src/components/LInks/index.jsx
+++ src/components/LInks/index.jsx
@@ -12,7 +12,7 @@ const Links = () => {
     <a href="https://www.linkedin.com/in/chrisma-caroline-stephen/" target="_blank" className='icon'><FontAwesomeIcon icon={faLinkedin} /></a>
     <a href="https://github.com/chrisma89"  target="_blank" className='icon'><FontAwesomeIcon icon={faGithub} /></a>
     <a href="mailto:chrismacaroline@gmail.com"  target="_blank" className='icon'><FontAwesomeIcon icon={faEnvelope} /></a>
-    <a href="https://docs.google.com/document/d/1hTBfm4X3syo_9xZpZ9r_Xojsw8e-u_sk/edit?usp=sharing&ouid=107425732819687011201&rtpof=true&sd=true"  target="_blank" className='icon'><FontAwesomeIcon icon={faFile} /></a>
+    <a href="/public/images/CV.pdf"  target="_blank" className='icon'><FontAwesomeIcon icon={faFile} /></a>
 
    </div>
   )
