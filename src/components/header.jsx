9d71720d0ffce4f36b018e090181ae59186a7dab
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-12 16:10:57 +0000

    header image hidden on smaller screens

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,7 +3,13 @@
 const Header = () => {
   return ( 
     <div className="container-fluid d-flex" style={{height : "80px"}}>
-       <img src="/images/CHRISMA.png" style={{height : "90%", width : "100%"}} />
+      <div className="d-none d-md-block w-100">
+        <img src="/images/CHRISMA.png" style={{height : "90%", width : "100%"}} />
+        </div>
+        <div className="d-block d-md-none w-100 text-left p-2" style={{ fontSize: "24px", fontFamily: "roboto-medium-italic" }}>
+        Chrisma Caroline Stephen
+      </div>
+       
     </div>
    
    );
