c6e9bfa0bdb6111ddd16e6977091f1bae63e91e4
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2024-03-12 16:14:21 +0000

    header styling changed to text cneter and margin mt-4 added

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="d-none d-md-block w-100">
         <img src="/images/CHRISMA.png" style={{height : "90%", width : "100%"}} />
         </div>
-        <div className="d-block d-md-none w-100 text-left p-2" style={{ fontSize: "24px", fontFamily: "roboto-medium-italic" }}>
+        <div className="d-block d-md-none w-100 text-center mt-4" style={{ fontSize: "24px", fontFamily: "roboto-medium-italic" }}>
         Chrisma Caroline Stephen
       </div>
        
