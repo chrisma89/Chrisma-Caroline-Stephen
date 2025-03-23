6830bd6838a33c6877e07adfd67e55121e900e04
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2025-03-23 21:27:25 +0000

    Change casing of the elements of the navbar, added UI/ux projects to the nav bar and renamed projects into front-end projects

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,35 +1,79 @@
-import Container from 'react-bootstrap/Container';
-import React, { useState } from 'react';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
-import './style.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-
-
+import Container from "react-bootstrap/Container";
+import React, { useState } from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import "./style.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
   // const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar  style={{position : "relative"}} expand="lg" className="container-fluid navbarmain">
-      <Container className='justify-content-end'>
-      {/* <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>Chrisma</Navbar.Brand> */}
+    <Navbar
+      style={{ position: "relative" }}
+      expand="lg"
+      className="container-fluid navbarmain"
+    >
+      <Container className="justify-content-end">
+        {/* <Navbar.Brand as={Link} to="/" onClick={() => setExpanded(false)}>Chrisma</Navbar.Brand> */}
 
-        <Navbar.Toggle  aria-controls="basic-navbar-nav" className="custom-toggler">
-        <FontAwesomeIcon icon={faBars} style={{ color: 'white' }} />
-</Navbar.Toggle>
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          className="custom-toggler"
+        >
+          <FontAwesomeIcon icon={faBars} style={{ color: "white" }} />
+        </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav" className="custom-toggler">
           <Nav>
             {/* <Link><img  style={{height :"100%", width : "40%"}}src="/images/logo.png"/></Link> */}
-          <Link style={{color:"white", paddingRight :"30px", fontFamily : "roboto-black-italic",textDecoration: "none"}} to="/">HOME</Link>
-            <Link style={{color:"white", paddingRight :"30px",fontFamily : "roboto-black-italic",textDecoration: "none"}} to="/projects" >PROJECTS</Link>
+            <Link
+              style={{
+                color: "white",
+                paddingRight: "30px",
+                fontFamily: "roboto-black-italic",
+                textDecoration: "none",
+              }}
+              to="/"
+            >
+              Home
+            </Link>
+            <Link
+              style={{
+                color: "white",
+                paddingRight: "30px",
+                fontFamily: "roboto-black-italic",
+                textDecoration: "none",
+              }}
+              to="/projects"
+            >
+              Front-End Projects
+            </Link>
+            <Link
+              style={{
+                color: "white",
+                paddingRight: "30px",
+                fontFamily: "roboto-black-italic",
+                textDecoration: "none",
+              }}
+              to="/uiuxprojects"
+            >
+              UI/UX Projects
+            </Link>
             {/* <Link style={{color:"white", paddingRight :"30px",fontFamily : "roboto-black-italic",textDecoration: "none"}} to="/links">LINKS</Link> */}
-            <Link style={{color:"white", paddingRight :"30px",fontFamily : "roboto-black-italic",textDecoration: "none"}} to="/contact">CONTACT</Link>
-          
+            <Link
+              style={{
+                color: "white",
+                paddingRight: "30px",
+                fontFamily: "roboto-black-italic",
+                textDecoration: "none",
+              }}
+              to="/contact"
+            >
+              Contact
+            </Link>
           </Nav>
-          </Navbar.Collapse>
-        
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
