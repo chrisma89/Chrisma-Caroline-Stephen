a8e3bd7f299921076e3ee1bcb579edf33c2c1809
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2025-03-24 21:42:48 +0000

    added card element to uiuxprojects jsx, imported svg into images and added image to the card

--- src/components/UIUXProjects/index.jsx
+++ src/components/UIUXProjects/index.jsx
@@ -1,8 +1,21 @@
-import { Container } from "react-bootstrap";
+import { Card, Container, Button } from "react-bootstrap";
+
 
 function UIUXProjects (){
   return(
-    <div>Test</div>
+    // <div>Test</div>
+    <Container>
+      <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="./images/Frame 1208.svg" />
+      <Card.Body>
+        <Card.Title>Favourites</Card.Title>
+        <Card.Text>
+          Placeholder for content
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </Container>
   )
 
 }
