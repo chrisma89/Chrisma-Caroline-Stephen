63a284c6c8e9cc0a3099bc45c6fe2999d1630292
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2025-03-25 23:06:29 +0000

    Styling applied to ui/ux projects page

--- src/components/UIUXProjects/index.jsx
+++ src/components/UIUXProjects/index.jsx
@@ -4,18 +4,20 @@ import { Card, Container, Button } from "react-bootstrap";
 function UIUXProjects (){
   return(
     // <div>Test</div>
-    <Container>
-      <Card style={{ width: '18rem' }}>
+    <div className= "container-fluid"style={{ backgroundColor : "#2C2C54"}} >
+    <Container className='container-fluid d-flex justify-content-center' style={{padding: "20px", backgroundColor : "#474787"}}>
+      <Card className ="container-fluid d-flex justify-content-center"style={{ width: '18rem' }}>
       <Card.Img variant="top" src="./images/Frame 1208.svg" />
-      <Card.Body>
+      <Card.Body className ="container-fluid justify-content-center">
         <Card.Title>Favourites</Card.Title>
         <Card.Text>
           Placeholder for content
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button style ={{backgroundColor:"#2C2C54"}}>View Details</Button>
       </Card.Body>
     </Card>
     </Container>
+    </div>
   )
 
 }
