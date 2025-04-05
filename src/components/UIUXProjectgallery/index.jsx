7b894dc9fe99f4d2b654556974b6c89c18d73b6a
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2025-04-05 23:14:08 +0100

    project gallery .map function added

--- src/components/UIUXProjectgallery/index.jsx
+++ src/components/UIUXProjectgallery/index.jsx
@@ -6,7 +6,7 @@ import UIUXProjectcard from "../UIUXProjectcard";
 
 const [selecteduiuxproject, setselecteduiuxproject] = useState(null)
 
-const selectuiuxproject = (uiuxprojectid) =>{
+const setuiuxproject = (uiuxprojectid) =>{
  const chosenuiuxproject = uiuxprojectsdata.find (uiuxproject => uiuxproject.id === uiuxprojectid );
  setselecteduiuxproject(chosenuiuxproject)
 }
@@ -16,10 +16,37 @@ return (
   <Container className='container-fluid' style={{padding: "20px", backgroundColor : "#474787"}}>
 {selecteduiuxproject ?
 (<>
+<UIUXProjectcard
+  id = {selecteduiuxproject.id}
+  Title={selecteduiuxproject.Title}
+  Description ={selecteduiuxproject.Description}
+    Image ={selecteduiuxproject.image}><div>{id}</div>
+<div>{Title}</div>
+<div>{Description}</div>
+<div>{Image}</div>
 
-<</>)
+</UIUXProjectcard>
+
+</>)
 :
-()}
+(
+  <Row>
+{uiuxprojectsdata.map((uiuxproject) => (
+                            <Col key={uiuxproject.id} sm={12} md={6} lg={4}>
+                                <UIUXProjects
+                                    id={uiuxproject.id}
+                                    description={uiuxproject.description}
+                                    // skill={project.skill}
+                                    image={uiuxproject.image}
+                                    setuiuxproject={() => setuiuxproject(uiuxproject.id)}
+                                />
+                            </Col>
+                        ))}
+                    </Row>
+ 
+)
+
+}
   </Container>
   </>
 )
