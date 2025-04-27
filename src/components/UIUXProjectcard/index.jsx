802522c7709b28bd674705463959283d300b40b5
Chrisma Stephen <149508742+chrisma89@users.noreply.github.com>
2025-04-27 22:35:52 +0100

    uiuxproject card fix

--- src/components/UIUXProjectcard/index.jsx
+++ src/components/UIUXProjectcard/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button, Container} from "react-bootstrap";
+import { Card, Button, Container, Row, Col} from "react-bootstrap";
 import UIUXProjects from "../UIUXProjects";
 import UIUXProjectsGallery from "../UIUXProjectgallery";
 
@@ -13,14 +13,19 @@ src
 }){
   return(
 <>
-<Container >
-<Card className='container-fluid d-flex align-items-center'>
+<Container className="projectcard">
+{/* <Row> */}
+        {/* <Col xs={12} sm={10} md={8} lg={6} xl={4}> */}
+         
+<Card className='container-fluid d-flex align-items-center mb-4'>
   <Card>{id}</Card>
   <Card.Img src={src}></Card.Img>
   <Card.Title> {Title}</Card.Title>
   <Card.Body>{Description}</Card.Body>
   <Button>{Buttonlabel}</Button>
 </Card>
+{/* </Col> */}
+{/* </Row> */}
 </Container>
 </>
   )
