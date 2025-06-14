1c2f6000833876036ec79f23f97e477f2588c432
Abish Stephen <chrismacaroline@gmail.com>
2025-06-14 16:21:49 +0100

    landing page text revision

--- src/components/HomeCard/index.jsx
+++ src/components/HomeCard/index.jsx
@@ -12,6 +12,7 @@ function HomeCard() {
           fontFamily: "roboto-regular-italic",
           paddingLeft: "0.2rem",
           paddingRight: "0.2rem",
+          paddingTop: "5rem",
           minHeight : "80vh"
         }}
       >
@@ -19,22 +20,41 @@ function HomeCard() {
           className="justify-content-start homecard-row"
           style={{
             fontFamily: "roboto-italic",
-            fontSize: "20px",
-            padding: "10px",
+            fontSize: "4rem",
+            // padding: "10px",
             paddingTop: "30px",
           }}
-        >
-          <Col>Welcome to Chrisma's portfolio page.</Col>
+        >Vanakkam, Swāgatham, Welcome.
+          {/* <Col>Vanakkam! Swāgatham! Welcome!
+</Col> */}
         </Row>
-        <Row
+        {/* <Row
           className="homecard-row"
           style={{
             fontFamily: "roboto-regular-italic",
-            fontSize: "20px",
+            fontSize: "3rem",
             padding: "10px",
           }}
         >
-          <Col>Get a glimpse of the story so far. </Col>
+          This portfolio is a collection of Front-end web development projects and UI/UX design work.
+        </Row> */}
+        <Row
+          className="justify-content-start homecard-row"
+          style={{
+            fontFamily: "roboto-regular-italic",
+            fontSize: "2.5rem",
+            paddingTop: "4rem",
+          }}
+        >I’m a front-end web developer who enjoys designing clean, user-friendly interfaces and bringing them to life with code. I use Figma for UI/UX design and JavaScript and React for development.
+        </Row>
+        <Row
+          className="justify-content-start homecard-row"
+          style={{
+            fontFamily: "roboto-regular-italic",
+            fontSize: "2.5rem",
+            paddingTop: "4rem",
+          }}
+        >I’d love to hear your opinion on these projects, so feel free to connect with me on LinkedIn and let’s collaborate.
         </Row>
         <Row
           className="homecard-row"
